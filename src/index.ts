07d63d1100def660024ed44779fefffb908fe9f8
Farid | Iamjustzero <imjustzero@duck.com>
2026-07-18 00:29:50 +0700

    Add helmet middleware for security headers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
 import dotenv from "dotenv";
 import { sequelize } from "./database";
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(farmRoutes);
 app.use(errorHandler);
